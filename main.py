d43e82e40c5166f756970ea223edc7a39ce9109c
Marat <m.muhametshin@rambler.ru>
2025-11-26 23:00:08 +0300

    Move factorial function to module and clean README

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 a = 1
 b = 2
 
-def factorial(n):
-	if n < 0:
-		raise ValueError("Factorial not defined for negative numbers")
-	result = 1
-	for i in range(2, n + 1):
-		result *= i
-	return result
+from factorial import factorial
 
 sum_ab = a + b
 first_factorial = factorial(sum_ab)
